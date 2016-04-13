dc085ea4643ee294cdb08cb0dbb7948ae5f10e2f
muhamd sohaib <msohaib980@gmail.com>
2016-04-12 23:00:54 -0700

    mvp is done

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', init);
+// variables
+var countright = 0;
+var countWrong = 0;
+var totalquestions = 0;
 
 function init(){
   creatingNumbers();
@@ -15,11 +19,13 @@ function init(){
 
 //crating Random Numbers
 function creatingNumbers(){
+  // var operator = ['-','+']
+  randomOperator = Math.floor(Math.random()*2)
   randNum1 = Math.floor(Math.random()*9+1)
   randNum2 = Math.floor(Math.random()*9+1)
   document.getElementById('random1').textContent=randNum1;
   document.getElementById('random2').textContent=randNum2;
-  randomSum = randNum1+randNum2
+  randomSum = randNum1  + randNum2
   console.log(randomSum);
 }
 
@@ -35,17 +41,28 @@ function numButtonsClicked(){
 function getAnswer(){
   var displayText=document.getElementById('resultDisplay')
   var text1 = userAnswer.textContent;
-  userSum = eval(text1)
+  userSum = eval(text1);
   // comparing random and user sum
   if(userSum===randomSum){
-    displayText.textContent = 'right'
+    displayText.textContent = 'right';
+    countright +=1;
+    // displaying correct answer in score board
+    document.getElementById('rightcount').textContent= 'Correct Answers = ' + countright;
   }else{
-    displayText.textContent = 'Nice try, correct answer is ' +  randomSum + '. Try again'
-  }
+    displayText.textContent = 'Nice try, correct answer is ' +  randomSum + '. Try again';
+    countWrong +=1;
+      // displaying wrong answer in score board
+    document.getElementById('wrongcount').textContent= 'Wrong Answers = ' + countWrong;
+}
   // clearing the random and user numbers after 2 second.
   var timeoutID = window.setTimeout(function() {
     // alert("Hello World!");
     creatingNumbers()
-    userAnswer.textContent =''
+    userAnswer.textContent ='';
+    totalquestions += 1;
+    document.getElementById('totalquestions').textContent= 'Total Number of Questions = ' + totalquestions;
+
 }, 2000)
 }
+
+// score board
